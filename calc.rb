becbd3e4adf8e5ef84a4573a51ebc075c42ad918
Ashish Gupta <ashish@quippp.com>
2013-10-15 20:20:07 +0530

    Chapter 5, Full name greeting

--- calc.rb
+++ calc.rb
@@ -2,3 +2,17 @@ puts 365*24
 puts 10 *365 *24 *60
 puts 30 *365 *24*60*60
 puts 1160000000/(60*60*24*365)
+
+puts 'Full name greeting'
+puts 'Hello there, what\'s your first name?'
+first_name = gets.chomp
+puts 'Your first name is ' + first_name
+puts 'Hello there, what\'s your middle name?'
+middle_name = gets.chomp
+puts 'Your middle name is ' + middle_name
+puts 'Hello there, what\'s your last name?'
+last_name = gets.chomp
+puts 'Your middle name is ' + last_name
+puts 'Your full name is ' + first_name +' '+ middle_name + ' ' + last_name
+
+
