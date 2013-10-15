7e321984cdde997f2d204fdc4ef8dc9efcba3137
Ashish Gupta <ashish@quippp.com>
2013-10-15 21:17:30 +0530

    Full name length

--- calc.rb
+++ calc.rb
@@ -1,11 +1,17 @@
+puts 'Full name greeting'
+puts 'Hello there, what\'s your first name?'
+first_name = gets.chomp
+puts 'Your first name is ' + first_name
+puts 'Hello there, what\'s your middle name?'
+middle_name = gets.chomp
+puts 'Your middle name is ' + middle_name
+puts 'Hello there, what\'s your last name?'
+last_name = gets.chomp
+puts 'Your middle name is ' + last_name
+puts 'Your full name is ' + first_name +' '+ middle_name + ' ' + last_name
+full_length = first_name.length + middle_name.length + last_name.length
+puts 'The length of your full name is ' + full_length.to_s
 
-puts 'Bigger, better favorite number'
-puts 'Hello, what\'s your favorite number?'
-favorite_number = gets.chomp
-puts 'Your favorite number is ' + favorite_number
-puts 'Lets add 1 to to your favorite number'
-bigger_number = favorite_number.to_i + 1
-puts 'The better number is ' + bigger_number.to_s
 
 
 
