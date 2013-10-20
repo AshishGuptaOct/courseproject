0a2fcc1eff56b23c81415ab8ed24cbd341dcbd7e
Ashish Gupta <ashish@quippp.com>
2013-10-21 04:58:18 +0530

    99 Bottles of Beer. Chapter 7, problem 1

---
+++ chap7.rb
@@ -0,0 +1,16 @@
+puts '99 Bottles of Beer on the Wall'
+num_at_start = 99
+num_now = num_at_start
+
+while num_now >2
+	puts num_now.to_s + ' bottles of beer on the wall' +
+	num_now.to_s + ' bottles of beer' 
+	num_now = num_now -1
+
+	puts 'Take one down, pass it around, ' + 
+	num_now.to_s + ' bottles of beer on the wall!'
+end
+
+puts '2 bottles of beer on the wall, 2 bottles of beer!'
+puts '1 bottle of beer on the wall'
+puts 'Take one down, pass it around, no more bottles of beer on the wall!'
