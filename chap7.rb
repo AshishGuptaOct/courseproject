e97d1fab9cf197405cd93b28cd3eed32e731f9f7
Ashish Gupta <ashish@quippp.com>
2013-10-21 06:35:43 +0530

    chap7, problem - Deaf Grandma

--- chap7.rb
+++ chap7.rb
@@ -14,3 +14,23 @@ end
 puts '2 bottles of beer on the wall, 2 bottles of beer!'
 puts '1 bottle of beer on the wall'
 puts 'Take one down, pass it around, no more bottles of beer on the wall!'
+
+
+puts 'Deaf Grandma'
+puts 'Say something to Grandma'
+
+
+while true
+	you_speak = gets.chomp
+
+	if you_speak == you_speak.upcase
+		year = 1930 + rand(21)
+		puts 'NO, NOT SINCE ' + year.to_s
+	else puts 'HUH?! SPEAK UP, SONNY!'
+	end
+
+	if you_speak == 'BYE'
+		break
+	end
+end
+	puts 'Bye Sonny'
