6ccc2e99e9d385754fac7d77188f30dd6064cb79
Ashish Gupta <ashish@quippp.com>
2013-10-31 02:45:07 +0530

    upto Question 5

--- ruby_exercises.rb
+++ ruby_exercises.rb
@@ -33,4 +33,78 @@ number_arrary.each do |num|
 		end
 end
 
-puts new_number_array
+new_number_array.select do |num|
+	if num %2 !=0
+		puts num
+	end
+end
+
+
+puts '3. Select odd numbers from #2. Working of the original array'
+
+number_array =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+number_array.select do |num|
+	if num %2 !=0
+		puts num
+	end
+end
+
+puts '4. Append 11'
+
+number_array =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+number_array.select do |num|
+	if num %2 !=0
+	end
+end
+
+number_array.push 11
+
+puts number_array
+
+puts '4. Prepend 0 to the beginning'
+
+
+number_array [0] = 0
+
+puts number_array
+
+puts '4. Append 11'
+
+number_array =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+number_array.select do |num|
+	if num %2 !=0
+	end
+end
+
+number_array.push 11
+
+puts number_array
+
+puts '4. Prepend 0 to the beginning'
+
+number_array [0] = 0
+
+puts number_array
+
+
+puts '5. Get rid of 11'
+
+number_array.pop 
+
+puts number_array
+
+puts '5. And append 3'
+
+number_array.push 3
+
+puts number_array
+
+
+
+
+
+
+
